d602e875790ee5335801ad6c59b484ae0c393af6
Gio <giovanni.lodi42@gmail.com>
2014-07-11 16:58:35 +0200

    Updated example

--- Example/Example/AppDelegate.h
+++ Example/Example/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  Example
-//
-//  Created by Gio on 11/07/2014.
-//  Copyright (c) 2014 mokagio. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
